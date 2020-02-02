629d8a9f8284d4e1ef8e00e235af4923a69e52e3
antenehs <antenehseifu@gmail.com>
2020-02-01 20:13:24 -0600

    Notificaitons with settings done.

---
+++ GuestParking/Managers/SettingsManager.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsManager.swift
+//  GuestParking
+//
+//  Created by Anteneh Sahledengel on 2/1/20.
+//  Copyright © 2020 Anteneh Sahledengel. All rights reserved.
+//
+
+import Foundation
